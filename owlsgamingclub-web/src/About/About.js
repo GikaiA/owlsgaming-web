c65376ced7cf43fe36acda62ac06a58bbd20e770
Gikai Andrews <gnaboys@gmail.com>
2023-10-01 23:02:10 -0400

    adjusted bgs of all games to minimal scroll and updated about section

--- owlsgamingclub-web/src/About/About.js
+++ owlsgamingclub-web/src/About/About.js
@@ -1,13 +1,20 @@
-import React from 'react'
-import "./About.css"
+import React from "react";
+import "./About.css";
 
 function About() {
   return (
-    <div className='about-section'>
-      <p>The Owls Esports Gaming Club is established for the purpose of bringing together people who share a common appreciation of video games and playing them to the best of their ability. This organization will also function as the nexus for students to compete on behalf of Florida Atlantic University in Esports game titles.</p>
-      
+    <div className="about-section-bg-wrapper">
+      <div className="about-section">
+        <h1>
+          The Owls Esports Gaming Club is established for the purpose of
+          bringing together people who share a common appreciation of video
+          games and playing them to the best of their ability. This organization
+          will also function as the nexus for students to compete on behalf of
+          Florida Atlantic University in Esports game titles.
+        </h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- owlsgamingclub-web/src/Homepage/Homepage.js
+++ owlsgamingclub-web/src/Homepage/Homepage.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import Typewriter from 'typewriter-effect';
-// import BackgroundSlider from 'react-background-slider';
 import './Homepage.css';
 import ColorTransition from '../ColorTransition/ColorTransition';
 
 function Homepage() {
- 
-  // const images = [
-  //   '../images/codphoto.jpg',
-  //   '../images/leaguephoto.jpg',
-  //   '../images/smashphoto.jpg',
-  //   '../images/valphoto.jpg',
-  //   '../images/owphoto.jpg'
-  // ];
 
   return (
     <div className={`homepage-section fade-in`}>
     <div className='homepage-section'>
       <ColorTransition/>
-      {/* <BackgroundSlider images={images} duration={5} transition={2}/> */}
       <p className='homepage-title'>Owls Gaming Club</p>
       <Typewriter className="custom-typewriter" options={{
               strings: ['Competitive', 'Community', 'Innovative'],
